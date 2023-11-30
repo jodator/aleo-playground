0e31a2f4af6e189b7b9293a776a8cbecc1133077
Maciej Gołaszewski <jodator@jodator.net>
2023-11-30 15:26:00 +0100

    🧹 Cleanup component

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -1,48 +1,22 @@
-import { FC, ReactElement, useMemo, useState } from 'react'
+import { FC, ReactNode, useMemo } from 'react'
 import { WalletProvider } from "@demox-labs/aleo-wallet-adapter-react"
 import { WalletModalProvider, WalletMultiButton } from "@demox-labs/aleo-wallet-adapter-reactui"
 import { LeoWalletAdapter } from "@demox-labs/aleo-wallet-adapter-leo"
 import { DecryptPermission, WalletAdapterNetwork } from "@demox-labs/aleo-wallet-adapter-base"
-
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import "@demox-labs/aleo-wallet-adapter-reactui/styles.css"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <Wallet>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-
-      <div>
-        <WalletMultiButton />
-      </div>
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <WalletMultiButton />
     </Wallet>
   )
 }
 
 export default App
 
-export const Wallet: FC<{ children: ReactElement[] }> = ({ children }) => {
+export const Wallet: FC<{ children: ReactNode }> = ({ children }) => {
   const wallets = useMemo(
     () => [
       new LeoWalletAdapter({
