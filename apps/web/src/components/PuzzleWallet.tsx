3b508725ae3bcf3bc5ea327969f424922d1e1668
Maciej Gołaszewski <jodator@jodator.net>
2023-12-01 15:53:06 +0100

    👱🏾‍♀️ Extract `shortenAddress()`

--- apps/web/src/components/PuzzleWallet.tsx
+++ apps/web/src/components/PuzzleWallet.tsx
@@ -1,14 +1,6 @@
 import { useAccount, useConnect, useDisconnect } from '@puzzlehq/sdk'
 import { useCallback } from 'react'
-
-const shortenAddress = (address: string) => {
-  const length = 5
-  if (address.length < length * 2) return address
-  return `${address.slice(0, length + 'aleo1'.length)}...${address.slice(
-    address.length - length,
-    address.length,
-  )}`
-}
+import { shortenAddress } from '@/utils/shortenAddress.tsx'
 
 export function PuzzleWallet() {
   const { connect } = useConnect()
