49d36950e4d12a582222682e6816111d1030737c
Maciej Gołaszewski <jodator@jodator.net>
2023-12-01 16:11:18 +0100

    🏝️ Refactor `<AleoCredits />`

--- apps/web/src/components/AleoCredits.tsx
+++ apps/web/src/components/AleoCredits.tsx
@@ -1,20 +1,38 @@
-import { useBalance } from '@puzzlehq/sdk'
+import { useAccount, useBalance } from '@puzzlehq/sdk'
+import { ReactNode, useMemo } from 'react'
 
 export function AleoCredits() {
-  const { balances } = useBalance()
+  const { account } = useAccount()
+  const { balances, loading } = useBalance()
 
-  const total = balances.reduce((acc, balance) => {
-    return {
-      public: acc.public + balance.public,
-      private: acc.private + balance.private,
-    }
-  }, { public: 0, private: 0 })
+  const total = useMemo(() => {
+    return (balances ?? []).reduce((acc, balance) => {
+      return {
+        public: acc.public + balance.public,
+        private: acc.private + balance.private,
+      }
+    }, { public: 0, private: 0 })
+  }, [JSON.stringify(balances)])
+
+  if (!account) return (
+    <Wrapper>
+      Not connected
+    </Wrapper>
+  )
+
+  if (loading) return (
+    <Wrapper>
+      Loading...
+    </Wrapper>
+  )
 
   return (
-    <div className="justify-end flex p-2 text-orange-100 text-sm">
+    <Wrapper>
       <span>Public: <strong>{total.public}</strong></span>
       <span className="mx-2 border-r-orange-300 border-r-2" />
       <span>Private: <strong>{total.private}</strong></span>
-    </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = ({ children }: { children: ReactNode }) => (<div className="justify-end flex p-2 text-orange-100 text-sm">{children}</div>)
