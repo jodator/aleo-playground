8878cce364c934efa4c5648918ad709b55007d37
Maciej Gołaszewski <jodator@jodator.net>
2023-12-05 11:47:58 +0100

    🛹 Try fetch public debt

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,20 +5,25 @@ import { LeoWalletPlayground } from '@/components/LeoWalletPlayground.tsx'
 import { PuzzleWallet } from '@/components/PuzzleWallet.tsx'
 import { AleoCredits } from '@/components/AleoCredits.tsx'
 import { Debts } from '@/components/Debts.tsx'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <div className="container md mx-auto">
-      <PuzzleWallet />
-      <AleoCredits />
-      <Debts />
+    <QueryClientProvider client={queryClient}>
+      <div className="container md mx-auto">
+        <PuzzleWallet />
+        <AleoCredits />
+        <Debts />
 
-      <hr className="border-t-2 mt-10 border-slate-500" />
-      <PuzzleWalletPlayground />
+        <hr className="border-t-2 mt-10 border-slate-500" />
+        <PuzzleWalletPlayground />
 
-      <hr className="border-t-2 mt-10 border-slate-500" />
-       <LeoWalletPlayground />
-    </div>
+        <hr className="border-t-2 mt-10 border-slate-500" />
+        <LeoWalletPlayground />
+      </div>
+    </QueryClientProvider>
   )
 }
 
--- apps/web/src/components/Debts.tsx
+++ apps/web/src/components/Debts.tsx
@@ -1,10 +1,19 @@
 import { useProgramRecords } from '@/hooks/useProgramRecords.ts'
 import { shortenAddress } from '@puzzlehq/sdk'
 import { useCallback } from 'react'
+import { useQuery } from '@tanstack/react-query'
 
 export function Debts() {
   const { records, loading } = useProgramRecords('iou_token_v001.aleo')
+  const { data } = useQuery({
+    queryKey: ['publicDebt'],
+    queryFn: async () => {
+      const response = await fetch('https://api.explorer.aleo.org/v1/testnet3/program/iou_token_v001.aleo/mapping/publicDebt/aleo1q6qstg8q8shwqf5m6q5fcenuwsdqsvp4hhsgfnx5chzjm3secyzqt9mxm8')
+      return response.json()
+    },
+  })
 
+  console.log('Query data', data)
 
   const onReveal = useCallback((record: unknown) => {
     console.log('reveal', record)
@@ -15,7 +24,8 @@ export function Debts() {
   return (
     <div className="flex flex-row gap-4">
       {records.map(({ data }) => (
-        <div key={data._nonce} className="border-2 p-8 border-orange-600 rounded basis-1/4 grow-0 shrink overflow-ellipsis text-orange-900 bg-orange-200">
+        <div key={data._nonce}
+             className="border-2 p-8 border-orange-600 rounded basis-1/4 grow-0 shrink overflow-ellipsis text-orange-900 bg-orange-200">
           amount: {data.amount}<br />
           issuer: {shortenAddress(data.issuer)}
           <br />
